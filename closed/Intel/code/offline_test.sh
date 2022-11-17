aba7c3d92fe3152bd88b9e066a7871e00f67c6dc
YuningQiu <113460727+YuningQiu@users.noreply.github.com>
2022-11-17 12:17:17 -0600

    Update offline_test.sh

--- closed/Intel/code/offline_test.sh
+++ closed/Intel/code/offline_test.sh
@@ -1,8 +1,3 @@
-cd 3d-unet-99.9/pytorch-cpu-kits19/
-../../run_docker.sh ce03546c4d75  /workspace/run_offline_accuracy.sh /opt/workdir/code/3d-unet-99.9/pytorch-cpu-kits19 aws_3dunet  3dunet_acc.txt
-../../run_docker.sh ce03546c4d75  /workspace/run_offline_performance.sh /opt/workdir/code/3d-unet-99.9/pytorch-cpu-kits19 aws_3dunet  3dunet_perf.txt
-cd ../../
-
 cd resnet50/pytorch-cpu/
 ../../run_docker.sh a17e4ea194cc  /workspace/run_offline_accuracy.sh  /opt/workdir/code/resnet50/pytorch-cpu aws_rn50  resnet50_acc.txt
 ../../run_docker.sh a17e4ea194cc  /workspace/run_offline.sh  /opt/workdir/code/resnet50/pytorch-cpu aws_rn50  resnet50_perf.txt
@@ -14,5 +9,12 @@ cd dlrm-99.9/pytorch-cpu/
 cd ../../
 
 cd retinanet/pytorch-cpu/
-../../run_docker.sh 5dfdb372208a  /workspace/run_offline.sh  /opt/workdir/code/retinanet/pytorch-cpu aws_ret  retinanet.txt
+../../run_docker.sh 5dfdb372208a  /workspace/run_offline_accuracy.sh  /opt/workdir/code/retinanet/pytorch-cpu aws_ret  retinanet_acc.txt
+../../run_docker.sh 5dfdb372208a  /workspace/run_offline.sh  /opt/workdir/code/retinanet/pytorch-cpu aws_ret  retinanet_perf.txt
 cd ../../
+
+cd 3d-unet-99.9/pytorch-cpu-kits19/
+../../run_docker.sh ce03546c4d75  /workspace/run_offline_accuracy.sh /opt/workdir/code/3d-unet-99.9/pytorch-cpu-kits19 aws_3dunet  3dunet_acc.txt
+../../run_docker.sh ce03546c4d75  /workspace/run_offline_performance.sh /opt/workdir/code/3d-unet-99.9/pytorch-cpu-kits19 aws_3dunet  3dunet_perf.txt
+cd ../../
+
