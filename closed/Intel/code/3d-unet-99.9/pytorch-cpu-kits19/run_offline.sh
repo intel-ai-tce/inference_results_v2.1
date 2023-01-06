addb4d8c281443e964de5b6f13c9ccb10ce1db53
yuning <113460727+YuningQiu@users.noreply.github.com>
2023-01-06 15:43:52 -0600

    Update run_offline.sh

--- closed/Intel/code/3d-unet-99.9/pytorch-cpu-kits19/run_offline.sh
+++ closed/Intel/code/3d-unet-99.9/pytorch-cpu-kits19/run_offline.sh
@@ -4,9 +4,15 @@ number_threads=`nproc --all`
 echo $number_threads
 number_instance=$((number_threads/8))
 echo $number_instance
-bash run_mlperf.sh --type=$1 \
+
+if [ -z "${RUN_TYPE}" ]; then
+    echo "NO RUN_TYPE ( perf/acc)found export RUN_TYPE=perf"
+    export RUN_TYPE=perf
+fi
+echo $RUN_TYPE
+bash run_mlperf.sh --type=${RUN_TYPE} \
 	           --precision=int8 \
-		   --user-conf=/workplace/user.conf \
+		   --user-conf=/workspace/user.conf \
 		   --num-instance=$number_instance \
 		   --cpus-per-instance=4 \
                    --scenario=Offline
