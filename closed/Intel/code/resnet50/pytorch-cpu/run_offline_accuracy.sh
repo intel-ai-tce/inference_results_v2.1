310fe2b9e6825fe52160141a29fea90fdcad0ba6
yuning <113460727+YuningQiu@users.noreply.github.com>
2023-01-12 11:07:03 -0600

    Update run_offline_accuracy.sh

--- closed/Intel/code/resnet50/pytorch-cpu/run_offline_accuracy.sh
+++ closed/Intel/code/resnet50/pytorch-cpu/run_offline_accuracy.sh
@@ -47,9 +47,9 @@ fi
 
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
 
-#export LD_PRELOAD=${CONDA_PREFIX}/lib/libjemalloc.so
+export LD_PRELOAD=${CONDA_PREFIX}/lib/libjemalloc.so
 
-#export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libiomp5.so
+export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libiomp5.so
 
 KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
